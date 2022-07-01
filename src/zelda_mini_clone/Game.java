cc5728dad1549161f52178bebdf2924fc8972a57
SoloRustyCoder <joao.campos@sou.fae.br>
2022-07-01 19:10:03 -0300

    Adding some Rendering logic

--- src/zelda_mini_clone/Game.java
+++ src/zelda_mini_clone/Game.java
@@ -1,36 +1,60 @@
 package zelda_mini_clone;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
 
 	private static final long serialVersionUID = 1L;
-	public static int WIDTH = 480, HEIGTH = 480;
+	public static int WIDTH = 480, HEIGHT = 480;
 	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	}
 	
+	public void Tick() {
+	}
+	
+	public void Render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.red);
+		g.fillRect(0, 0, 50, 50);
+		bs.show();
+	}
+	
 	public static void main(String[] args) {
 		Game game = new Game();
 		JFrame frame = new JFrame();
-		
 		frame.add(game);
 		frame.setTitle("Mini-Zelda");
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
 		new Thread(game).start();
 	}
 
 	@Override
 	public void run() {
 		while(true) {
-			System.out.println("Jogo rodando");
+			Tick();
+			Render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
